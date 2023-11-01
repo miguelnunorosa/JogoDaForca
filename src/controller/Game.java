6339b12e8088035c8b074555e943874cf6dfad8b
Miguel Rosa <miguelrosa.programacao@gmail.com>
2023-11-01 16:42:08 +0000

    Start devel game class

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -2,17 +2,10 @@ package controller;
 
 import settings.Common;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Game {
 
     Common utils = new Common();
+
 
 
     public Game() {
@@ -23,8 +16,9 @@ public class Game {
 
 
     public void startGame(){
-        //
+
     }
+
 
 
 
--- src/model/Word.java
+++ src/model/Word.java
@@ -2,7 +2,27 @@ package model;
 
 public class Word {
 
-    //
+    private final String[] wordsList = {"Java", "Rio", "Serra", "Computador", "Developer"};
+
+    public Word() {}
+
+
+
+    /**
+     * This method obtain total array words (total of gaming words to guess)
+     * @return total of words (length) existent on array list length
+     */
+    public int getWordListLenght(){ return wordsList.length; }
+
+
+
+    /**
+     * Method to get the word position and name
+     * @param wordPosition get the word position
+     * @return note position/name
+     */
+    public String getWordPositionAndName(int wordPosition) { return wordsList[wordPosition]; }
+
 
 
 }
